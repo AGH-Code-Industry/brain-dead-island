df6ad8ce5609740b1bda2af447e2d89ba3fd368e
Challmymind <81416266+Challmymind@users.noreply.github.com>
2024-03-19 20:40:15 +0100

    Adjusted and extended boilerplate code, backend agnostic code moved from traits to structures.

--- bdi_game/src/renderer/mod.rs
+++ bdi_game/src/renderer/mod.rs
@@ -2,5 +2,8 @@
 
 /// Rendering pipeline traits.
 pub mod traits;
+/// Rendering pipeline structures.
+/// Implementation agnostic.
+pub mod structures;
 /// SDL2 implentation of the rendering pipeline.
 pub mod sdl;
--- bdi_game/src/renderer/sdl.rs
+++ bdi_game/src/renderer/sdl.rs
@@ -1,14 +1,39 @@
+use super::traits;
+
 /// Initializes video submodules, create window, handle canvas.
+pub struct  RenderBuilderSDL{
+
+}
+
+/// Manages clusters and renders on screen.
 pub struct RenderSDL{
 
 }
 
-/// Groups objects to be rendered. 
-pub struct ClusterSDL{
+impl traits::RenderBuilder<RenderSDL> for RenderBuilderSDL{
+
+    fn new() -> Self {
+        todo!();
+    }
+
+    fn build(self) -> RenderSDL {
+        todo!();
+    }
+
+    fn window(self, name : String, width : u32, heigth : u32) -> Self {
+       todo!();
+    }
 
 }
 
-/// Describes properties of the rendered object.
-pub struct UnitSDL{
+impl traits::Render for RenderSDL{
+
+    fn create_cluster(&self) -> super::structures::Cluster {
+       todo!();
+    }
+
+    fn render(cluser : super::structures::Cluster) {
+        
+    }
 
 }

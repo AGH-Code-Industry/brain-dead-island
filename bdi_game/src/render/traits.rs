61de65936e6832b9adeca8074815c6c8864cdfe8
Challmymind <81416266+Challmymind@users.noreply.github.com>
2024-03-21 20:38:40 +0100

    Created better structure with associated types instead of generics, window creation in example is working

--- bdi_game/src/render/traits.rs
+++ bdi_game/src/render/traits.rs
@@ -3,7 +3,14 @@ use super::structures::Cluster;
 /// Initializes video submodules, create window, handle canvas.
 /// Moves all data to the Renderer and destroys itself.
 // we can't make RenderBuilder and Render backend agnostic.
-pub trait RenderBuilder<T : Render>{
+//
+// DISCLAIMER
+// i have no idea how i cooked it : )
+// but it's working
+pub trait RenderBuilder{
+
+    type Unit<'a>;
+    type Render<'a> : Render<Unit<'a> =  Self::Unit<'a>>;
 
     /// Creates Renderer.
     fn new() -> Self;
@@ -12,17 +19,19 @@ pub trait RenderBuilder<T : Render>{
     fn window(self, name : &str, width : u32, heigth : u32) -> Self;
 
     /// Builds window
-    fn build(self) -> T;
+    fn build<'a>(self) -> Self::Render<'a>;
 
 }
 
 pub trait Render{
+
+    type Unit<'a>;
     
     /// Creates cluster.
-    fn create_cluster(&self) -> Cluster;
+    fn create_cluster<'a>(&self) -> Cluster<Self::Unit<'a>>;
 
     /// Renders cluster.
     // moved here to keep Cluster backend agnostic.
-    fn render(&mut self, cluser : &Cluster);
+    fn render<'a>(&mut self, cluser : &Cluster<Self::Unit<'a>>);
 
 }

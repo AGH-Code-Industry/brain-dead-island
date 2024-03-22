54f536e19cfdb7dc03df43ac760b0003f52240c2
Challmymind <81416266+Challmymind@users.noreply.github.com>
2024-03-22 16:36:22 +0100

    basic drawing implemented, example is working

--- bdi_game/src/render/sdl.rs
+++ bdi_game/src/render/sdl.rs
@@ -3,8 +3,20 @@ use sdl2;
 
 /// SDL unit
 pub struct UnitSDL<'a>{
-    position : &'a (u32,u32),
-    texture : &'a sdl2::render::Texture<'a>,
+    pub position : &'a (i32,i32),
+    pub texture : Option<&'a sdl2::render::Texture<'a>>,
+    pub color : Option<sdl2::pixels::Color>,
+}
+
+impl <'a>UnitSDL<'a>{
+    pub fn new(position : &'a (i32,i32)) -> Self{
+
+        Self{
+            position,
+            texture : None,
+            color :None
+        }
+    }
 }
 
 /// Initializes video submodules, create window, handle canvas.
@@ -24,6 +36,7 @@ pub struct RenderSDL{
 
     builder : RenderBuilderSDL,
     canvas : sdl2::render::WindowCanvas,
+    tile : sdl2::rect::Rect,
 
 }
 
@@ -53,9 +66,11 @@ impl RenderBuilder for RenderBuilderSDL{
             .into_canvas()
             .build()
             .expect("Cannot create canvas");
+        let tile = sdl2::rect::Rect::new(0,0,100,100);
         RenderSDL{
             builder : self,
-            canvas
+            canvas,
+            tile,
         }
     }
 
@@ -76,7 +91,23 @@ impl Render for RenderSDL{
         super::structures::Cluster::new()
     }
 
-    fn render<'a>(&mut self, cluser : &super::structures::Cluster<Self::Unit<'a>>) {
+    fn render<'a>(&mut self, cluster : &super::structures::Cluster<Self::Unit<'a>>) {
+
+        self.canvas.set_draw_color(sdl2::pixels::Color::WHITE);
+        for unit in &cluster.objects{
+
+            self.tile.set_x(unit.position.0);
+            self.tile.set_y(unit.position.1);
+            if let Some(color) = unit.color{
+                self.canvas.set_draw_color(color);
+            }
+            // We should CATCH this error.
+            // Add handling here.
+            self.canvas.fill_rect(self.tile).unwrap();
+
+        } 
+
+        self.canvas.present();
         
     }
 

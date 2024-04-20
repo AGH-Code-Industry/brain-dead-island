ba4e067af7e2589fd8dfa4c086dae2e9124fbe7d
Michał Lasoń <81416266+Challmymind@users.noreply.github.com>
2024-04-20 18:37:58 +0200

    Rendering (#1)
    * Rendering pipeline boilerplate
    
    * Adjusted and extended boilerplate code, backend agnostic code moved from traits to structures.
    
    * Corrected functions' signatures, created examples directory, added render example, created "how to" use examples
    
    * Created better structure with associated types instead of generics, window creation in example is working
    
    * basic drawing implemented, example is working
    
    * used cargo fmt on rendering pipeline
    
    * make sure sdl2::Sdl isn't hidden by renderer
    
    * renamed render module to be more verbose, changed tick in game.rs to be non-blocking
    
    * implemented bulk_add and made better example
    
    * changed filling to the enum, created direct on screen drawing (bypass clusters)
    
    * added new dependency sdl2_gfx (in Cargo.toml are only rust bindings, provide library yourself), created boilerplate hexagon drawing
    
    * completed direct rendering for squares and hexagons (still no textures), updated example
    
    * add texture rendering to the square, example and simple texture
    
    * revert GameDisplay removal
    
    * add README
    
    * apply formatting
    
    ---------
    
    Co-authored-by: Stanisław Borowy <stabor@startmail.com>

---
+++ bdi_game/src/display/structures.rs
@@ -0,0 +1,24 @@
+/// Groups objects to be rendered.
+pub struct Cluster<T> {
+    // list of objects inside cluster.
+    pub objects: Vec<T>,
+}
+
+/// Backend agnostic functions.
+impl<T> Cluster<T> {
+    /// Adds unit to the cluster.
+    pub fn add(&mut self, unit: T) {
+        self.objects.push(unit);
+    }
+
+    /// Adds units to the cluster.
+    pub fn bulk_add(&mut self, units: Vec<T>) {
+        self.objects.extend(units)
+    }
+
+    pub fn new() -> Self {
+        Self {
+            objects: Vec::new(),
+        }
+    }
+}

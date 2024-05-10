88450b9248ac284e1d8658a8ef1b0752bdb4f58f
JakubG-git <jakub.macius@gmail.com>
2024-05-10 19:17:16 +0200

    remove useless functions

--- bdi_game/src/simulation/entity_actions_queue.rs
+++ bdi_game/src/simulation/entity_actions_queue.rs
@@ -2,14 +2,6 @@ use super::grid::GridPoint;
 use std::collections::VecDeque;
 pub enum EntityCommand {
     GoTo { destination: GridPoint },
-    AcquireResource { destination: GridPoint }, //chop wood, mine stone, mine iron, mine gold, acquire food
-    PickUpResource { destination: GridPoint },
-    StoreResource { destination: GridPoint },
-    Attack { target: GridPoint },
-    Build { destination: GridPoint },
-    Eat,
-    Drink,
-    Sleep,
 }
 
 pub type EntityActionsQueue = VecDeque<EntityCommand>;
--- bdi_game/src/simulation/entity_state.rs
+++ bdi_game/src/simulation/entity_state.rs
@@ -2,15 +2,4 @@
 pub enum EntityState {
     Idle,
     Moving,
-    AcquiringResource,
-    PickingUpResource,
-    StoringResource,
-    Attacking,
-    Building,
-    Eating,
-    Drinking,
-    Sleeping,
-    RunningAway,
 }
-
-pub type EntityStateHistory = Vec<EntityState>;
